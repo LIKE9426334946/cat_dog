d2263dbaff405e57532c8e59162fd5c57e308a9a
The Noart <101931052+LIKE9426334946@users.noreply.github.com>
2026-04-19 15:06:27 +0800

    Update trainer.py

--- utils/trainer.py
+++ utils/trainer.py
@@ -1,10 +1,13 @@
 import torch
+from tqdm import tqdm
 
 def train_one_epoch(model, loader, optimizer, criterion, device):
     model.train()
     total_loss = 0
 
-    for x, y in loader:
+    pbar = tqdm(loader, desc=f"Train Epoch {epoch}", leave=False)
+
+    for x, y in pbar:
         x, y = x.to(device), y.float().to(device)
 
         optimizer.zero_grad()
@@ -14,6 +17,9 @@ def train_one_epoch(model, loader, optimizer, criterion, device):
         optimizer.step()
 
         total_loss += loss.item()
+        pbar.set_postfix({
+            "loss": f"{loss.item():.4f}"
+        })
 
     return total_loss / len(loader)
 
@@ -22,6 +28,8 @@ def eval_model(model, loader, criterion, device):
     total_loss = 0
     correct = 0
     total = 0
+
+    pbar = tqdm(loader, desc=f"Val Epoch {epoch}", leave=False)
 
     with torch.no_grad():
         for x, y in loader:
@@ -33,5 +41,11 @@ def eval_model(model, loader, criterion, device):
             preds = (torch.sigmoid(out) > 0.5).int()
             correct += (preds == y.int()).sum().item()
             total += y.size(0)
+            acc = correct / total
+            
+            pbar.set_postfix({
+                "loss": f"{loss.item():.4f}",
+                "acc": f"{acc:.4f}"
+            })
 
     return total_loss / len(loader), correct / total
